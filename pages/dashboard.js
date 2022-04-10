f9fe0b6483ad1ac0e7e712fc31c3a9aea06efe26
hamim <rahmanhamiminfo@gmail.com>
2022-04-10 23:05:17 +0600

    private route

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,38 @@
-function Dashboard() {
- return <h1>Dashboard page</h1>;
+import React, { useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+
+export default function Dashboard() {
+ const { status } = useSession();
+
+ useEffect(() => {
+  if (status === "unauthenticated") signIn();
+ }, [status]);
+
+ if (status !== "authenticated") {
+  return <h2>Loading...</h2>;
+ }
+
+ return (
+  <>
+   <div>Dashboard</div>
+  </>
+ );
 }
 
-export default Dashboard;
+// import { useSession, signIn } from "next-auth/react";
+
+// export default function Dashboard() {
+//  const { status } = useSession({
+//   required: true,
+//   onUnauthenticated() {
+//    // The user is not authenticated, handle it here.
+//    signIn();
+//   },
+//  });
+
+//  if (status === "loading") {
+//   return "Loading or not authenticated...";
+//  }
+
+//  return "Dashboard";
+// }
