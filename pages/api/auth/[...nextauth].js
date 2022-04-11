75a2a9643660aff3ed8a7266f392e07e69427baa
Hamim <rahmanhamiminfo@gmail.com>
2022-04-11 14:43:26 +0600

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,4 +8,25 @@ export default NextAuth({
    clientSecret: process.env.GITHUB_SECRET,
   }),
  ],
+ // database: process.env.DB_URL,
+ // session: {
+ //  jwt: true,
+ // },
+ // jwt: {
+ //  secret: "ngerjsdfjor",
+ // },
+ // callbacks: {
+ //  async jwt({ token, user }) {
+ //   if (user) {
+ //    token.id = user.id;
+ //   }
+ //   return token;
+ //  },
+ //  async session({ session, token }) {
+ //   session.user.id = token.id;
+ //   return session;
+ //  },
+ // },
+
+ // these code is deprecated
 });
